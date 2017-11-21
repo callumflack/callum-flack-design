af35bd6c9cbd2b4b802893ef2c6639116d6d13af
Callum Flack <callumflack@gmail.com>
2017-11-21 11:47:18 +1000

    setup basic mailchimp form

--- components/FormNews.vue
+++ components/FormNews.vue
@@ -1,8 +1,32 @@
 <template lang="pug">
-  form(action='https://formspree.io/vince@oceanblueboats.com.au', method='post', @submit.prevent='handleSubmit') 
+  form(
+    action="https://patternworks.us13.list-manage.com/subscribe/post?u=027f0219cdd96399fb792993e&id=f1f20b580e",
+    method="post", 
+    name="mc-embedded-subscribe-form", 
+    @submit.prevent="handleSubmit"
+  )
     .Form-group.Meta
-      input(placeholder='Your email address', type='email', name='email', v-model='email', required)
-      button(:class='buttonClass', type='submit', :disabled='loading || formSent') {{buttonText}}
+      input(
+        placeholder="Your email address", 
+        type="email", 
+        name="EMAIL", 
+        value="", 
+        v-model="email", 
+        required
+      )
+      div(style="position: absolute; left: -5000px;", aria-hidden="true")
+        input(
+          type="text", 
+          name="b_027f0219cdd96399fb792993e_f1f20b580e", 
+          tabindex="-1", 
+          value=""
+        )
+      button(
+        :class="buttonClass", 
+        type="submit", 
+        name="subscribe", 
+        :disabled="loading || formSent",
+      ) {{buttonText}}
 </template>
 
 <script>
@@ -21,12 +45,7 @@ export default {
 
   computed: {
     buttonClass() {
-      return [
-        "Button",
-        {
-          "u-bgColorTransparent": this.plainNewsletterButton
-        }
-      ];
+      return ["Button"];
     },
     buttonText() {
       if (this.loading) {
