88ac0688a8f3c3504fc0dbc09c8815658e50dcd0
Callum Flack <callumflack@gmail.com>
2018-02-02 19:42:59 +1000

    add: mobile-only video play button (1st draft)

--- components/MarkdownMovie.vue
+++ components/MarkdownMovie.vue
@@ -3,7 +3,7 @@
     .FlexEmbed(:class="[{ 'figure--frame': frame }]")
       .FlexEmbed-ratio.device-ratio
       .FlexEmbed-content
-        video.device-position(autoplay="", loop="", :poster="poster")
+        video.device-position(autoplay, loop, :poster="poster", ref="videoref")
           source(
             :src="src",
             type="video/mp4"
@@ -13,6 +13,10 @@
           <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 670 1381">
             <path class="cls-1" d="M670,102C670,45.8,624.35,0,568.15,0H101.67C45.47,0,0,45.8,0,102V1279.91C0,1336.11,45.47,1381,101.67,1381H568.15c56.2,0,101.85-44.89,101.85-101.09ZM628.31,1210.53H41.69V168.24H628.31Z"/>
           </svg>
+        .video-button-wrapper(:class="[{ 'u-hiddenVisually': isActive }]")
+          button.video-button(@click="play")
+            span.icon.icon--play-button
+              <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" viewBox="0 0 80 80" xml:space="preserve"><path d="M40 0C17.9 0 0 17.9 0 40c0 22.1 17.9 40 40 40s40-17.9 40-40C80 17.9 62.1 0 40 0zM40 78C19 78 2 61 2 40S19 2 40 2s38 17 38 38S61 78 40 78z"></path><polygon class="st0" points="33.9 51.4 51.1 39.6 33.9 28 "></polygon></svg>
 </template>
 
 <script scoped>
@@ -34,6 +38,22 @@ export default {
           "device--desktop": this.desktop
         }
       ];
+    }
+  },
+  data() {
+    return {
+      isActive: false
+    };
+  },
+  methods: {
+    play: function() {
+      this.$refs.videoref.play();
+      this.isActive = !this.isActive;
+
+      // if (video.paused) {
+      // } else {
+      //   this.$refs.videoref.pause();
+      // }
     }
   },
   props: {
@@ -58,6 +78,30 @@ export default {
 
 .frame {
   border: 1px solid var(--color-border);
+}
+
+.video-button-wrapper {
+  align-items: center;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  left: 0;
+  position: absolute;
+  right: 0;
+  top: 0;
+
+  @media (--small-viewport) {
+    display: none;
+  }
+}
+
+.video-button {
+  color: var(--color-bg);
+}
+
+.icon--play-button {
+  height: 74px;
+  width: 74px;
 }
 
 /* 
