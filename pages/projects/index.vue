9a9c50ffe44d820f72af9dc8b521b916c17431b7
Callum Flack <callumflack@gmail.com>
2018-08-17 10:35:15 +1000

    content: swap "work" dir for "projects" dir

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,62 +1,134 @@
 <template lang="pug">
-  main.Container(role="main")
-    post-excerpt(
-      v-if="post.published && post.category === 'projects'"
-      v-for="post in posts"
-      :key="post.permalink"
-      :link="post.permalink"
-      :heroImage="post.heroImage"
-      :title="post.title"
-      :date="post.date"
-      :readingtime="post.readingtime"
-      :lede="post.lede"
-    )
-    
-    //- .InterludeSpace
-      hr
-      .block--my7
-        p.Meta.Meta--isParagraph.c-textLight.u-textCenter.m-xA.u-size3of4
-          span.u-block
-            nuxt-link.u-linkClean(to="/about") Callum Flack
-            | &nbsp;is writing about the things inbetween. Here's the most recent posts:
-      hr
+.Container
+  header(role="header")
+    h1.Supertitle Selected projects
+    // p.Subheading.fw-regular We ask our clients to identify key performance goals and collaborate directly with us in a process of iterative design, rapid prototyping, ongoing testing, and optimization.
+    .Project-filter.b-pb3
+      .u-flex.u-flexJustifyBetween
+        .Meta.m-a0 Filter by: 
+        .Meta.c-textLight.m-a0
+          a.u-linkClean(
+            v-for="(tag, index) in tags"
+            @click.prevent="filterProjectsByTag(tag)"
+            href="#"
+            :class="[{ 'c-highlight': tag === activeTag }]"
+          )
+            span {{ tag }}
+            //- span(v-if="index !== tags.length - 1") ,&nbsp;
+            //- span(v-else) .
+      // hr.m-t3
+  
+  main(role="main")
+    .Extract-super
+      .FlexGrid.FlexGrid--noGrow
+        Project(
+          v-if="project.published && project.category === 'projects'"
+          v-for="project in visibleProjects"
+          :key="project.permalink"
+          :link="project.permalink"
+          :title="project.title"
+          :lede="project.lede"
+          :image="project.thumbImage"
+          :cloudinary="project.thumbInCloudinary"
+          :year="project.year"
+          :tags="project.tags"
+        )
+
+    <svg class='u-hiddenVisually'>
+      <filter id='blur' color-interpolation-filters='sRGB' x='0' y='0' height='100%' width='100%'>
+        <feGaussianBlur stdDeviation="33" />
+        //- Clip edge artifacts:
+        //- https://stackoverflow.com/questions/24416564/svg-blur-filter-edge-artifacts
+        <feComponentTransfer>
+          <feFuncA type="discrete" tableValues="1 1"/>
+        </feComponentTransfer>
+      </filter>
+    </svg>
 </template>
 
 <script>
-import PostExcerpt from "~/components/PostExcerpt.vue";
+import Project from "~/components/Project2.vue";
+
+const allTagName = "All";
+
+// Functions are defined outside component since asyncData does not have access
+// to the component because it's called before initializing the component
+
+const trimTags = tags => tags.map(tag => tag.trim());
+
+function projectTagStringToList(projectTags) {
+  const trimmedProjectTags = projectTags.endsWith(",")
+    ? projectTags.substring(0, projectTags.length - 1)
+    : projectTags;
+  return trimTags(trimmedProjectTags.split(","));
+}
 
 export default {
+  name: "projects",
   components: {
-    PostExcerpt
+    Project
   },
   head() {
     return {
-      title: "Blog – Callum Flack Design",
+      title: "Work – Callum Flack Design",
       meta: [
         {
           hid: "description",
           name: "description",
-          content:
-            "Selected writing by Callum Flack about digital design, user experience & code"
+          content: "Selected digital design & development projects by Callum Flack since 2014"
         }
       ]
     };
   },
   data() {
     return {
-      posts: []
+      projects: [],
+      visibleProjects: [],
+      tags: [],
+      activeTag: allTagName
     };
   },
+  methods: {
+    filterProjectsByTag(tag) {
+      this.activeTag = tag;
+
+      if (tag === allTagName) {
+        this.visibleProjects = this.projects;
+        return;
+      }
+
+      this.visibleProjects = this.projects.filter(project => {
+        if (!project.tags) return false;
+        const projectTags = trimTags(project.tags.split(","));
+        return projectTags.includes(tag);
+      });
+    }
+  },
   async asyncData({ app }) {
-    const posts = await app.$content("/posts").getAll();
+    /* const projects = await app.$content("/projects").getAll(); */
+    const projects = await app.$content("/posts").getAll();
+
+    const uniqueTags = projects.filter(project => project.tags).reduce(
+      (tags, project) => {
+        const projectTags = projectTagStringToList(project.tags);
+        const uniqueProjectTags = projectTags.filter(tag => !tags.includes(tag));
+
+        return [...tags, ...uniqueProjectTags];
+      },
+      [allTagName]
+    );
+
+    uniqueTags.sort();
 
     return {
-      posts
+      projects,
+      visibleProjects: projects,
+      tags: uniqueTags
     };
   }
 };
 </script>
 
-<style scope>
+<style scope lang="css">
 @import "../../assets/styles/variables.css";
 </style>
