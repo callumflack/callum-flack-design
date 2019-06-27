4005e702a26f95e9d6e69ed16788a83d375c4ece
Barry Hood <barryph@protonmail.com>
2019-06-28 01:47:38 +1200

    Group posts by year

--- src/layouts/projects.vue
+++ src/layouts/projects.vue
@@ -19,26 +19,38 @@
                   @click="goToIndex(1)"
                   :class="{ 'is-active': activeIndex === 1 }"
                   class="Tab-controls--button Meta"
-                >Archive</button>
+                >Featured</button>
               </li>
             </ul>
           </div>
         </div>
 
         <SliderSlides class="Grid-extract--posts">
-          <SliderSlide v-if="featuredPosts && featuredPosts.length > 0">
-            <Card
-              v-for="post in featuredPosts"
-              :post="post"
-              :key="post.attributes.permalink"
-            ></Card>
+          <SliderSlide v-if="featuredPosts && Object.keys(featuredPosts).length > 0">
+            <div
+              v-for="year in Object.keys(featuredPosts).reverse()"
+              :key="year"
+            >
+              <h3 class="Title block mt-12 mb-4">{{ year }}</h3>
+              <Card
+                v-for="post in featuredPosts[year]"
+                :post="post"
+                :key="post.attributes.permalink"
+              ></Card>
+            </div>
           </SliderSlide>
-          <SliderSlide v-if="allPosts && allPosts.length > 0">
-            <Card
-              v-for="post in allPosts"
-              :post="post"
-              :key="post.attributes.permalink"
-            ></Card>
+          <SliderSlide v-if="allPosts && Object.keys(allPosts).length > 0">
+            <div
+              v-for="year in Object.keys(allPosts).reverse()"
+              :key="year"
+            >
+              <h3 class="Title block mt-12 mb-4">{{ year }}</h3>
+              <Card
+                v-for="post in allPosts[year]"
+                :post="post"
+                :key="post.attributes.permalink"
+              ></Card>
+            </div>
           </SliderSlide>
           <!-- <SliderSlide v-if="devPosts && devPosts.length > 0">
             <Card
@@ -72,22 +84,60 @@ export default {
 
   props: ["page"],
 
+  methods: {
+    sortByDate(a, b) {
+      new Date(b.attributes.date) - new Date(a.attributes.date);
+    },
+    groupByYearMethod(byYear, post) {
+      const year = parseInt(new Date(post.attributes.date).getFullYear(), 10);
+
+      // Set up list for year if it doesn't already exist
+      if (!byYear[year]) {
+        byYear[year] = [];
+      }
+
+      byYear[year].push(post);
+
+      return byYear;
+    },
+    /*
+     * Transform array of post into object grouping
+     * posts by their year.
+     *
+     * @param {array} posts
+     * @return {object}
+     */
+    groupByYear(posts) {
+      return posts
+        // Unsure if saber is ordering by date
+        // If it is already you can remove this
+        .sort(this.sortByDate)
+        .reduce(this.groupByYearMethod, {});
+    },
+  },
+
   computed: {
-    allPosts() {
+    allPostsUngrouped() {
       return this.page.posts
         .filter(x => x.attributes.category === "project")
     },
+    allPosts() {
+      return this.groupByYear(this.allPostsUngrouped);
+    },
     featuredPosts() {
-      return this.allPosts
+      const posts = this.allPostsUngrouped
         .filter(x => x.attributes.tags.includes("featured"))
+      return this.groupByYear(posts);
     },
     designPosts() {
-      return this.allPosts
+      const posts = this.allPostsUngrouped
         .filter(x => x.attributes.tags.includes("UI"))
+      return this.groupByYear(posts);
     },
     devPosts() {
-      return this.allPosts
+      const posts = this.allPostsUngrouped
         .filter(x => x.attributes.tags.includes("code"))
+      return this.groupByYear(posts);
     },
   },
 };
