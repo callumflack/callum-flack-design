d012c960c83cc25967daa55e85182a799e78165a
Callum Flack <callumflack@gmail.com>
2019-07-17 21:17:34 +1000

    add: html lang attr

--- src/saber-browser.js
+++ src/saber-browser.js
@@ -1,33 +1,33 @@
-import './styles/application.css'
+import "./styles/application.css";
 
 /* https://saber.land/docs/manipulating-head.html#set-default-values */
 export default ({ setHead }) => {
   setHead({
+    htmlAttrs: { lang: "en" },
     meta: [
       {
         name: "google-site-verification",
-        content: "HN9v-wB-PoQjHk1CGpr5YVG9VUNrcOaOjHACfG3SSSI"
+        content: "HN9v-wB-PoQjHk1CGpr5YVG9VUNrcOaOjHACfG3SSSI",
       },
       {
         // pinterest
         name: "p:domain_verify",
-        content: "8280c00ce54aef1d74f05dc38cd9fb91"
+        content: "8280c00ce54aef1d74f05dc38cd9fb91",
       },
       // Twitter card
       { name: "twitter:card", content: "summary" },
       { name: "twitter:site", content: "@callumflack" },
       {
         name: "twitter:image",
-        content: "https://avatars2.githubusercontent.com/u/749885?s=460&v=4"
+        content: "https://avatars2.githubusercontent.com/u/749885?s=460&v=4",
       },
-      { name: "twitter:image:alt", content: "Callum Flack" }
-
+      { name: "twitter:image:alt", content: "Callum Flack" },
     ],
     // favicon
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     // polyfill
     script: [
-      { src: "https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver" }
-    ]
-  })
-}
+      { src: "https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver" },
+    ],
+  });
+};
