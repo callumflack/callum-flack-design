e55f287ac14591a18a25333e1b0360d1229cf605
Callum Flack <callumflack@gmail.com>
2019-09-27 15:34:51 +1000

    add: saber-plugin-netlify-redirect

--- saber-node.js
+++ saber-node.js
@@ -1,8 +1,9 @@
 // https://github.com/saberland/saber/blob/master/packages/saber-plugin-netlify-redirect/example/saber-node.js
-// exports.onCreatePages = function () {
+
+// exports.onCreatePages = function() {
 //   this.pages.createRedirect({
-//     fromPath: '/redirect-to-about',
-//     toPath: '/about.html',
-//     redirectInBrowser: true
-//   })
-// }
+//     fromPath: "/redirect-to-about",
+//     toPath: "/about.html",
+//     redirectInBrowser: true,
+//   });
+// };
