4ecae166441d3998764f63d7e37946ce8e9b1770
Callum Flack <callumflack@gmail.com>
2019-12-14 12:10:18 +1000

    Update PageTitle.vue
    * with intersection-observer for CFd logo invert on scroll
    * use of slots for hero image + extra text

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,43 +1,57 @@
 <template>
-  <header :class="headerLayoutStyle">
-    <nav
-      class="frame-area-a Heading Heading-4xl font-medium fixed z-50"
-      :class="headingInvertStyle"
-    >
-      <saber-link to="/">
-        <template v-if="$route.path !== '/'">
-          &#8592;
-        </template>
-        CFd
-      </saber-link>
-    </nav>
-    <div class="frame-area-b">
-      <h1 class="Heading Heading-4xl font-medium" :class="headingInvertStyle">
-        {{ page.title }}
-      </h1>
-      <PageTitleMeta
-        v-if="showMeta"
-        :page="page"
-        :invert="invert"
-        :class="textColorStyle"
-        class="Space-sm-t"
-      ></PageTitleMeta>
-      <p class="Text-xl Block-xs-t lg:w-5/6" :class="textColorStyle">
-        {{ page.lede }}
-        <template v-if="$route.path === '/'">
-          <saber-link to="/about" class="Link text-brand-red">
-            Learn more &#8594;
-          </saber-link>
-        </template>
-      </p>
-      <!-- slot -->
-      <slot></slot>
+  <header class="relative">
+    <!-- hero image slot -->
+    <slot name="image"></slot>
+    <!-- title block -->
+    <div class="relative" :class="headerLayoutStyle">
+      <nav
+        class="frame-area-a Heading Heading-4xl font-medium fixed z-50"
+        :class="[headingInvertStyle, scrollInvertStyle]"
+      >
+        <a v-if="$route.path === '/'" v-smooth-scroll="{ duration: 1000 }" href="#top">
+          CFd
+        </a>
+        <saber-link v-else to="/">
+          <!-- &#8592; -->
+          CFd
+        </saber-link>
+      </nav>
+      <div class="frame-area-b">
+        <h1 class="Heading Heading-4xl font-medium" :class="headingInvertStyle">
+          {{ page.title }}
+        </h1>
+        <PageTitleMeta
+          v-if="showMeta"
+          :page="page"
+          :invert="invert"
+          :class="textInvertStyle"
+          class="Space-sm-t"
+        ></PageTitleMeta>
+        <p
+          v-if="page.lede"
+          class="Text-xl Block-xs-t"
+          :class="[textInvertStyle, ledeStyle]"
+        >
+          {{ page.lede }}
+          <template v-if="$route.path === '/'">
+            <saber-link to="/about" class="Link text-brand-red">
+              Learn more &#8594;
+            </saber-link>
+          </template>
+        </p>
+        <!-- default slot -->
+        <slot></slot>
+      </div>
+      <div ref="observe" class="io-target"></div>
     </div>
   </header>
 </template>
 
 <script>
+import Vue from "vue";
 import PageTitleMeta from "../components/PageTitleMeta";
+import vueSmoothScroll from "vue-smooth-scroll";
+Vue.use(vueSmoothScroll);
 
 export default {
   components: {
@@ -54,23 +68,70 @@ export default {
       default: false,
     },
   },
+  data: () => ({
+    visible: false,
+  }),
   computed: {
     headerLayoutStyle() {
-      // return this.$route.path === "/" ? "Block" : "Block-t Block-md-b";
-      /* if (this.$route.path === "/") {
-        return "frame frame--intro Block";
-      } */
-      /* if (this.$route.path === "/about") {
-        return "frame frame--lede Block-t Block-md-b";
-      } */
+      if (this.$route.path === "/about") {
+        return "frame frame--lede Block-t Block-sm-b";
+      }
       return "frame frame--lede Block";
     },
     headingInvertStyle() {
       return this.invert && "text-white";
     },
-    textColorStyle() {
-      return this.invert ? "text-white opacity-75" : "text-gray-dark";
+    scrollInvertStyle() {
+      return !this.visible && "text-black-important";
     },
+    textInvertStyle() {
+      // return this.invert ? "text-white opacity-75" : "text-gray-dark";
+      if (this.$route.path === "/about") {
+        return "text-white";
+      } else if (this.invert) {
+        return "text-white opacity-75";
+      }
+      return "text-gray-dark";
+    },
+    ledeStyle() {
+      // return this.$route.path === "/" && "lg:w-5/6";
+      return null;
+    },
+  },
+  mounted() {
+    // use i-o to toggle CFd logo in <PageTitle> when it leaves the viewport
+    // https://www.notion.so/callum/Intersection-observer-use-in-Vue-ff50d8e8e79647b7bccfbe861b2eaf5b
+    const io = new IntersectionObserver(entries => {
+      // Log available data when an intersection happens
+      // console.log(entries);
+      // Element enters the viewport
+      if (entries[0].intersectionRatio !== 0) {
+        this.visible = true;
+        // Element leaves the viewport
+      } else {
+        this.visible = false;
+      }
+    });
+
+    // Elements to be observed
+    const box = this.$refs.observe;
+
+    // Start observing box
+    io.observe(box);
   },
 };
 </script>
+
+<style lang="postcss">
+/* IO thinga */
+.io-target {
+  @apply absolute;
+  bottom: 9vh;
+  height: 2px;
+  width: 2px;
+  background-color: salmon;
+}
+.text-black-important {
+  @apply text-black !important;
+}
+</style>
