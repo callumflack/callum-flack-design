6056ae97a683c12c9c4640945346ed942a650120
Callum Flack <callumflack@gmail.com>
2019-12-18 18:20:53 +1000

    Update formatDate.js

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,7 +1,14 @@
 import tinydate from "tinydate";
 
-// {YYYY}.{MM}.{DD} | moment("MMMM Do, YYYY")
-const format = tinydate("{MMMM} {Do} {YYYY}");
+// how do moment("MMMM Do, YYYY")?
+// https://github.com/lukeed/tinydate#patterns
+// this doesn't fucking work despite being straight form the docsæ
+// const format = tinydate("Today is: {MMMM} {DD}, {YYYY}", {
+//   MMMM: d => d.toLocaleString("default", { month: "long" }),
+//   DD: d => d.getDate(),
+// });
+
+const format = tinydate("{YYYY}.{MM}.{DD}");
 
 export default date => {
   return date instanceof Date ? format(date) : format(new Date(date));
